934c4ea60eb90de5fbda9967f2bf701e24f13a2c
Thomas Wiesner (thomaswiesner.com) <485781+tomw1808@users.noreply.github.com>
2024-02-03 08:01:02 +0100

    removed wallet options and formatted rainbowkit

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,15 +3,8 @@
 import * as React from 'react';
 import {
   RainbowKitProvider,
-  getDefaultWallets,
   connectorsForWallets,
-  Chain,
 } from '@rainbow-me/rainbowkit';
-import {
-  argentWallet,
-  trustWallet,
-  ledgerWallet,
-} from '@rainbow-me/rainbowkit/wallets';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
   sepolia,
@@ -19,52 +12,29 @@ import {
 import { publicProvider } from 'wagmi/providers/public';
 import { infuraProvider } from 'wagmi/providers/infura';
 
-const anvil: Chain = {
-    id: 31_337,
-    name: "Anvil Local",
-    network: "anvil",
-    nativeCurrency: {
-        decimals: 18,
-        name: "tETH",
-        symbol: "tETH"
-    },
-    rpcUrls: {
-        public: { http: ["http://localhost:8545"]},
-        default: { http: ["http://localhost:8545"]},
-    },
-    testnet: true
-}
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
-    // anvil,
     sepolia,
   ],
-  [infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_ID || "" })]
+  [process.env.NEXT_PUBLIC_INFURA_ID ? infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_ID }) : publicProvider()]
 );
 
-const projectId = 'YOUR_PROJECT_ID';
-
-const { wallets } = getDefaultWallets({
-  appName: 'RainbowKit demo',
-  projectId,
-  chains,
-});
+import {
+  injectedWallet,
+} from '@rainbow-me/rainbowkit/wallets';
 
 const demoAppInfo = {
-  appName: 'Rainbowkit Demo',
+  appName: 'Candide ERC 4337 Demo Application',
 };
 
 const connectors = connectorsForWallets([
-  ...wallets,
   {
-    groupName: 'Other',
+    groupName: "Wallet",
     wallets: [
-      argentWallet({ projectId, chains }),
-      trustWallet({ projectId, chains }),
-      ledgerWallet({ projectId, chains }),
-    ],
-  },
+      injectedWallet({ chains }),
+    ]
+  }
 ]);
 
 const wagmiConfig = createConfig({
@@ -79,7 +49,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   React.useEffect(() => setMounted(true), []);
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} appInfo={demoAppInfo}>
+      <RainbowKitProvider modalSize="compact" chains={chains} appInfo={demoAppInfo}>
         {mounted && children}
       </RainbowKitProvider>
     </WagmiConfig>
